22be35b6ef578ac2fd6a9e74cbd5db369bd5c20d
KJ <subodhsjagtap19@gmail.com>
2022-12-01 17:13:27 +0530

    completed next task

--- dom.js
+++ dom.js
@@ -13,3 +13,12 @@ head.style.border = "solid 3px #000";
 let item = document.querySelector("#main .title");
 item.style.fontWeight = "bold";
 item.style.color = "green";
+
+let title = document.getElementsByClassName("container")[0];
+// title.innerHTML = "<h1>This is Title</h1>";
+
+let lis = document.querySelectorAll("#items .list-group-item");
+lis[2].style.backgroundColor = "green";
+for(let li of lis){
+    li.style.fontWeight = "bold";
+}
