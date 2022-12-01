826e23a023ac7295b17291b9f2c1b28ccb8a7cf3
KJ <subodhsjagtap19@gmail.com>
2022-12-01 17:31:59 +0530

    querySelector task

--- dom.js
+++ dom.js
@@ -14,20 +14,25 @@ let item = document.querySelector("#main .title");
 item.style.fontWeight = "bold";
 item.style.color = "green";
 
-let title = document.getElementsByClassName("container")[0];
+// let title = document.getElementsByClassName("container")[0];
 // title.innerHTML = "<h1>This is Title</h1>";
 
-let lis = document.querySelectorAll("#items .list-group-item");
-lis[2].style.backgroundColor = "green";
-for(let li of lis){
-    li.style.fontWeight = "bold";
-}
+let li2 = document.querySelector("li:nth-child(2)");
+let li3 = document.querySelector("li:nth-child(3)");
+li2.style.backgroundColor = "green";
+li3.style.display = "none";
 
-let newLi = document.createElement("li");
-newLi.innerText = "Item 5";
-lis[0].parentElement.append(newLi);
+// let lis = document.querySelectorAll("#items .list-group-item");
 
-let lis2 = document.getElementsByTagName("li");
-for(let li of lis2){
-    li.style.backgroundColor = "lightgrey";
-}
+// for(let li of lis){
+//     li.style.fontWeight = "bold";
+// }
+
+// let newLi = document.createElement("li");
+// newLi.innerText = "Item 5";
+// lis[0].parentElement.append(newLi);
+
+// let lis2 = document.getElementsByTagName("li");
+// for(let li of lis2){
+//     li.style.backgroundColor = "lightgrey";
+// }
