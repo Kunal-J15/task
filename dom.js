1cd6776425370afee4666670d24e93b83d541a46
KJ <subodhsjagtap19@gmail.com>
2022-12-02 15:33:39 +0530

    traversing DOM

--- dom.js
+++ dom.js
@@ -22,11 +22,11 @@ item.style.color = "green";
 // li2.style.backgroundColor = "green";
 // li3.style.display = "none";
 
-let lis = document.querySelectorAll("#items .list-group-item");
-lis[2].style.color="green";
-for(let li=1;li<lis.length;li+=2){
-    lis[li].style.backgroundColor = "green";
-}
+// let lis = document.querySelectorAll("#items .list-group-item");
+// lis[2].style.color="green";
+// for(let li=1;li<lis.length;li+=2){
+//     lis[li].style.backgroundColor = "green";
+// }
 
 // let newLi = document.createElement("li");
 // newLi.innerText = "Item 5";
@@ -36,3 +36,47 @@ for(let li=1;li<lis.length;li+=2){
 // for(let li of lis2){
 //     li.style.backgroundColor = "lightgrey";
 // }
+
+// ...........traversing the DOM.............
+//parentNode
+let ul = document.querySelector("#items");
+ul.parentNode.style.backgroundColor= "gray";
+//childNodes
+//childElements
+//firstChild
+//firstElementChild
+//nextSibling
+//nextElementSibling
+//previousSibling
+//previousElementSibling
+
+//insetBefore(newDiv,h1)
+
+//createTextNode
+//createElement
+    //ele.classList.add
+    //setAttribute("title","val")
+
+//    1. Now go head and add HEllo word before Item Lister
+let text = document.createTextNode("HEllo word");
+let container = document.querySelector(".container");
+let h1 = document.querySelector("#header-title");
+container.insertBefore(text,h1)
+
+//    2. Now go head and add HEllo word before Item 1 If stuck for long check the hints.
+let text2 = document.createTextNode("HEllo word");
+ul.insertBefore(text2,ul.firstElementChild);
+/*parentElement
+lastelementchild
+lastchild
+createchild
+firstelementchild
+firstchild
+nextsibling
+nextelementsibling
+previoussibling
+previouselementsibling
+createelement
+setAttribute
+createtesxtnode
+appendchild*/
